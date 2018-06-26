ef96e7171a9b0826752c58069755714ff59e3dd0
Nikita Zamiatin <4406015@gmail.com>
2018-06-26 15:30:01 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,44 +1,62 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./style.css"
 
-console.log('hello world ')
+// console.log('hello world ')
 
 
 const user = document.querySelector('#apply-buttom');
 const searchInput = document.querySelector('#searchMap');
 var myLatlng;
 var map;
+const cors = "https://galvanize-cors.herokuapp.com/"
 
 user.addEventListener('click', (event) => {
     event.preventDefault();
     const searchTerm = searchInput.value
-    
+
     let userInputAddress = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=AIzaSyAzk29EWio3H7I6UvJ0gWJ8rwZeK4Nh8yU`
-    // let userCheckBoxURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=850&type=${types}&keyword=cruise&key=AIzaSyB386cmYAo7WGgHqAyzWMBTBBDCAr6ynDQ`
     myLatlng = userInputAddress
 
     fetch(userInputAddress)
 
-    .then(response => response.json())
-    .then(result =>{
-       var location = result.results["0"].geometry.location;
+        .then(response => response.json())
+        .then(result => {
+            var location = result.results["0"].geometry.location;
 
-        map.setCenter(location);
-        letsSeeIfChec();
+            map.setCenter(location);
+            const types = letsSeeIfChec();
+            // console.log(types)
+            let userCheckBoxURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=850&type=${types}&keyword=cruise&key=AIzaSyB386cmYAo7WGgHqAyzWMBTBBDCAr6ynDQ`
 
-        fetch()
+            fetch(cors + userCheckBoxURL)
+                .then(response => response.json())
+                .then(result => {
+                    var icon = result.results
+
+                    for (let i = 0; i < icon.length; i++) {
+                        console.log(icon[i].geometry.location)
+                    console.log(icon[i].icon)
+                        new google.maps.Marker({
+                            
+                            position: icon[i].geometry.location,
+                            map: map,
+                            icon : icon[i].icon,
+                           size : new google.maps.Size(20, 20)
+
+                        });
+                    }
 
 
+                })
 
-// fetch places API  with location and types  
-    })
-    .catch(error => console.error(error))
+                console.log(myLatlng)
+            console.log(userCheckBoxURL)
+        })
+        .catch(error => console.error(error))
 
-
-    
-    // getResults(searchTerm)   futere function with url 
     console.log(searchTerm)
-    console.log(myLatlng)
+        console.log(myLatlng)
+
 })
 
 
@@ -48,22 +66,23 @@ const buttonCheck = document.querySelector('.top_buttoms')
 buttonCheck.addEventListener('click', (event) => {
     const checkValue = event.target.id
 })
-let arr =[];
 
-let type = "";
 
 function letsSeeIfChec() {
+    let types = [];
     if (document.getElementById('restaurant').checked) {
-        type += "restaurant"
-        console.log("checked ")
-    } else if (document.getElementById('night_club').checked) {
-        type += "night_club"
-    } else if (document.getElementById('bar').checked) {
-        type += "bar"
-    } else {
-        type
+        types.push("restaurant")
+        // console.log("checked ")
     }
-    
+
+    if (document.getElementById('night_club').checked) {
+        types.push("night_club")
+    }
+
+    if (document.getElementById('bar').checked) {
+        types.push("bar")
+    }
+    return types.join(',')
 }
 
 
@@ -73,7 +92,7 @@ function letsSeeIfChec() {
 // type=restaurant // night_club // bar 
 // &keyword=cruise& /// not need 
 // key=AIzaSyAzk29EWio3H7I6UvJ0gWJ8rwZeK4Nh8yU // my key 
-
+// add pins to map
 function initMap() {
     // The location of myLatlng
     myLatlng = {
@@ -81,25 +100,24 @@ function initMap() {
         lng: -73.9712
     };
 
-        console.log(myLatlng)
+    console.log(myLatlng)
 
-    // The map, centered at myLatlng
-     map = new google.maps.Map(
+    map = new google.maps.Map(
         document.getElementById('map'), {
             zoom: 14,
             center: myLatlng
 
         });
-        // The marker, positioned at myLatlng  with they need !!!!
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            // icon : img
-        });
-    }
-    
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+    });
+
+
+}
 
 
 
 
-    initMap();
+
+initMap();
